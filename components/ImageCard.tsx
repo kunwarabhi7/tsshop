4fb7a6405455459f1709e98ad38c84fcd3895983
Abhishek <abhishekparmar162@gmail.com>
2023-03-18 13:28:30 +0530

    get data

---
+++ components/ImageCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ImageCard = (props: Props) => {
+  return (
+    <div>ImageCard</div>
+  )
+}
+
+export default ImageCard
--- pages/store.tsx
+++ pages/store.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import {  GetServerSideProps } from 'next'
 
-type Props = {}
 
-const store = (props: Props) => {
+interface MyPageProps {
+    product: string;
+  }
+
+const store = ({product}:MyPageProps) => {
+    console.log(product)
   return (
     <div>store</div>
   )
 }
 
 export default store
+
+export  const getServerSideProps : GetServerSideProps = async (contex)=> {
+const res = await fetch('https://dummyjson.com/products')   
+const products = await res.json()
+const product = products.products
+return{
+    props:{
+        product
+    }
+}
+}
